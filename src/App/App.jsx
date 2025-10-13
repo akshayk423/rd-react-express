ffda4b5a83a70a02f2e93202e787b56bd964eb18
akshayk423 <akshay.khalasi3@gmail.com>
2025-10-13 04:00:35 -0700

    moon hover effect

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <div className="page">
       <div className="page__background">
+<<<<<<< Updated upstream
         <img src={moon} alt="Moon" className="page__moon" />
         <img src={title} alt="RecommenDead" className="page__title" />
         <div className="page__searchbar-container">
@@ -17,6 +18,10 @@ const App = () => {
             </button>
           </form>
         </div>
+=======
+        <img src={moon} className="page__moon" />
+        <img src={title} className="page__title" />
+>>>>>>> Stashed changes
       </div>
     </div>
   );
