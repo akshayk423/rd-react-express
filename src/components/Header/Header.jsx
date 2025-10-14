43dd68fb44ba120f001bc5daf3982402e9ab31ca
akshayk423 <akshay.khalasi3@gmail.com>
2025-10-14 04:55:09 -0700

    avatar switch on blood theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
 import "./Header.css";
 import title from "../../assets/icons/logo-title.png";
 import avatar from "../../assets/icons/avatar.png";
+import avatarDark from "../../assets/icons/avatar-dark.png";
 import MoonToggle from "../MoonToggle/MoonToggle";
+import AppContext from "../../contexts/AppContext";
+import { useContext } from "react";
 
 function Header() {
+  const contextData = useContext(AppContext);
+  const isNight = contextData.isNight;
+
   return (
     <div className="header__content">
-      <img src={avatar} className="header__avatar" />
+      <img
+        src={`${isNight ? avatar : avatarDark}`}
+        className="header__avatar"
+      />
       <img src={title} className="page__title" />
       <MoonToggle />
     </div>
