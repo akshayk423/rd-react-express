dabfc2cf2f8b630c04f14ccc104ac2e2d959d016
akshayk423 <akshay.khalasi3@gmail.com>
2025-10-15 00:06:00 -0700

    search bar switches themes with blood and night themes

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,33 @@
 import "./SearchBar.css";
 import plusGrey from "../../assets/icons/plus-icon-grey.svg";
 import plusWhite from "../../assets/icons/plus-icon-white.svg";
+import AppContext from "../../contexts/AppContext";
+import { useContext } from "react";
 
 const SearchBar = () => {
+  const { isNight } = useContext(AppContext);
+
   return (
     <div className="searchbar">
-      <form className="searchbar__form searcbar__form_type_night">
-        <input className="searchbar__input" type="text" />
+      <form
+        className={`searchbar__form ${
+          isNight ? "searchbar__form_type_night" : "searchbar__form_type_blood"
+        }`}
+      >
+        <input
+          className={`searchbar__input ${
+            isNight
+              ? "searchbar__input_type_night"
+              : "searchbar__input_type_blood"
+          }`}
+          type="text"
+          placeholder="Type a keyword — slasher, actor, or decade — to summon your next horror recommendation."
+        />
         <button className="searchbar__submit" type="submit">
           <img
             className="searchbar__icon"
-            src={plusGrey}
-            alt="plus-icon-grey"
+            src={isNight ? plusGrey : plusWhite}
+            alt="plus icon"
           />
         </button>
       </form>
