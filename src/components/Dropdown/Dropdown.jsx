231349c1cc1d77f527088cfe26cebe61977a0199
akshayk423 <akshay.khalasi3@gmail.com>
2025-10-15 01:43:22 -0700

    imported dropdown component to header

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,17 +1,27 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useContext } from "react";
 import "./Dropdown.css";
+import AppContext from "../../contexts/AppContext";
 
+// trigger is the element that opens the menu, items is the options for the menu
 function Dropdown({ trigger, items }) {
+  //context variable for current theme
+  const contextData = useContext(AppContext);
+  const isNight = contextData.isNight;
+
+  //useState and ref logic to handle opening and closing of dropdown
   const [open, setOpen] = useState(false);
   const ref = useRef(null);
 
+  // on mount
   useEffect(() => {
+    // listener function for checking if the click event is on the current element
     const closeOnOutsideClick = (e) => {
       if (ref.current && !ref.current.contains(e.target)) {
         setOpen(false);
       }
     };
     document.addEventListener("click", closeOnOutsideClick);
+    //removes the listener after unmounted
     return () => document.removeEventListener("click", closeOnOutsideClick);
   }, []);
 
@@ -23,7 +33,7 @@ function Dropdown({ trigger, items }) {
   };
 
   return (
-    <div className="dropdown" ref={ref}>
+    <div className={`dropdown`} ref={ref}>
       <button
         className="dropdown__trigger"
         onClick={() => setOpen((prev) => !prev)}
@@ -32,7 +42,11 @@ function Dropdown({ trigger, items }) {
       </button>
 
       {open && (
-        <ul className="dropdown__menu">
+        <ul
+          className={`dropdown__menu ${
+            isNight ? "dropdown__menu_type_night" : "dropdown__menu_type_blood"
+          }`}
+        >
           {items.map((item, i) => (
             <li
               key={i}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.css";
-import Dropdown from "../../components/Dropdown/Dropdown.jsx";;
+import Dropdown from "../../components/Dropdown/Dropdown.jsx";
 import title from "../../assets/icons/logo-title.png";
 import avatar from "../../assets/icons/avatar.png";
 import avatarDark from "../../assets/icons/avatar-dark.png";
@@ -30,10 +30,25 @@ function Header() {
 
   return (
     <div className="header__content">
-      <img
-        src={`${isNight ? avatar : avatarDark}`}
-        className="header__avatar"
+      <Dropdown
+        trigger={
+          <img
+            src={`${isNight ? avatar : avatarDark}`}
+            className="header__avatar"
+          />
+        }
+        items={[
+          {
+            onClick: console.log("View Recommendations"),
+            label: "View Recommendations",
+          },
+          {
+            onClick: console.log("Sign Out"),
+            label: "Sign Out",
+          },
+        ]}
       />
+
       <img src={title} className="page__title" />
       <MoonToggle />
     </div>
