11ed82a02fe0c78320abe01c51342b5ed6baab13
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-10-16 03:53:18 -0500

    Added more to api calls

--- src/utils/moviesApi.js
+++ src/utils/moviesApi.js
@@ -8,8 +8,8 @@ export const getKeyword = (tag, apiKey) => {
   ).then(checkResponse);
 };
 
-export const getMovie = (keywordId, apiKey) => {
+export const getMovie = (keywordId, apiKey, page = 1) => {
   return fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27&with_keywords=${keywordId}&language=en-US`
+    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27&with_keywords=${keywordId}&language=en-US&page=${page}`
   ).then(checkResponse);
 };
